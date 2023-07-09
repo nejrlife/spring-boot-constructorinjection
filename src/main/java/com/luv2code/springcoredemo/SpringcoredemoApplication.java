7e4fa85c755d386deebc0ce1d862ab164a085a56
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-07-09 21:45:20 +0800

    Added packages

--- src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//(		scanBasePackages = {"com.luv2code.springcoredemo", "com.luv2code.util"}
+//)
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
