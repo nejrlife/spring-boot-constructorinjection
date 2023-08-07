5afcfcda2110d46f05922a3a97a00c12da36f763
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-08-07 22:33:34 +0800

    Added default security

--- src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-@OpenAPIDefinition(servers = {@Server(url = "https://sows-object-tracker.uc.r.appspot.com", description = "Default Server URL")})
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 
+@SecurityScheme(type = SecuritySchemeType.APIKEY, name = "api_key", in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(servers = {@Server(url = "https://sows-object-tracker.uc.r.appspot.com", description = "Default Server URL")}, info = @Info(title = "Apply Default Global SecurityScheme in springdoc-openapi", version = "1.0.0"), security = { @SecurityRequirement(name = "api_key") })
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
