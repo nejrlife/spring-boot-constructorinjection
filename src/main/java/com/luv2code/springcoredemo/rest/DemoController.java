f38804611c7403a18653f0106cf46ea9b4e22278
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-08-07 23:56:04 +0800

    Test Fix

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,8 +1,12 @@
 package com.luv2code.springcoredemo.rest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Description;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,18 +21,29 @@ public class DemoController {
 	private Coach myCoach;
 	
 	//define a constructore for dependency injection
-	@GetMapping("/")
+	@GetMapping(path = "/", produces=MediaType.APPLICATION_JSON_VALUE)
 	@Operation(summary = "Hello World", description = "Hello worldy")
 	@Description(value = "This is just a hello worldy")
-	public String hello() {
-		return "hello world!";
+	public Map<String, String> hello() {
+		
+		HashMap<String, String> map = new HashMap<>();
+	    map.put("key", "value");
+	    map.put("foo", "bar");
+	    map.put("aa", "bb");
+        
+        return map;
 	}
 	
-	@GetMapping("/getAString")
+	@GetMapping(path = "/getAString", produces=MediaType.APPLICATION_JSON_VALUE)
 	@Operation(summary = "Get String", description = "Get a random string")
 	@Description(value = "This is just to get a random string.")
-	public String getAString() {
-		return "sample string";
+	public Map<String, String> getAString() {
+		HashMap<String, String> map = new HashMap<>();
+	    map.put("key", "value");
+	    map.put("foo", "bar");
+	    map.put("getAString", "getAString");
+        
+        return map;
 	}
 	
 	@Autowired
