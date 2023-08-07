7e03e8faa27ac5157dbcde7a923a289e692daa87
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-08-07 20:11:26 +0800

    HTTP to HTTPS

--- src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/luv2code/springcoredemo/SpringcoredemoApplication.java
@@ -1,7 +1,12 @@
 package com.luv2code.springcoredemo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 public class SpringcoredemoApplication {
