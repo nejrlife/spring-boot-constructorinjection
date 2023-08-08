fdeb82e67020d60a9f1d0786dda9a3a0801b913d
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-08-09 00:48:52 +0800

    Initial Commit

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Description;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @RestController
 public class DemoController {
@@ -17,7 +20,6 @@ public class DemoController {
 	//define a constructore for dependency injection
 	@GetMapping(path = "/", produces=MediaType.APPLICATION_JSON_VALUE)
 	@Operation(summary = "Hello World", description = "Hello worldy")
-	@Description(value = "This is just a hello worldy")
 	public Map<String, String> hello() {
 		
 		HashMap<String, String> map = new HashMap<>();
@@ -27,13 +29,15 @@ public class DemoController {
         return map;
 	}
 	
-	@GetMapping(path = "/getAString", produces=MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Get String", description = "Get a random string")
-	@Description(value = "This is just to get a random string.")
-	public Map<String, String> getAString() {
+	@GetMapping(path = "/getAString", produces=MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Get String", description = "Get a randoms string")
+	public Map<String, String> getAString(@Parameter(description = "Object to add.", required = true,
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema=@Schema(implementation = ReqBody.class))) ReqBody body) {
 		HashMap<String, String> map = new HashMap<>();
 		map.put("Status", "OK");
 	    map.put("StatusCode", "200");
+	    map.put("Message", body != null && body.getMyFile() != null ? body.getMyFile() : "null value");
         
         return map;
 	}
@@ -41,5 +45,17 @@ public class DemoController {
 	@Autowired
 	public DemoController () {
 		System.out.println("In construcor: ");
+	}
+	
+	public class ReqBody {
+		private String myFile;
+		
+		public String getMyFile () {
+	        return myFile;
+	    }
+
+	    public void setMyFile (String myFile) {
+	    	this.myFile = myFile;
+	    }
 	}
 }
