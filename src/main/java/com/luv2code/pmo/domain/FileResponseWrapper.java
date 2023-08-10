cb2a6b4f1bd8fee2bc6bb41bf7c7f091d2a05299
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-08-10 18:26:24 +0800

    FIxes

---
+++ src/main/java/com/luv2code/pmo/domain/FileResponseWrapper.java
@@ -0,0 +1,15 @@
+package com.luv2code.pmo.domain;
+
+public class FileResponseWrapper {
+	
+	private String responseString;
+
+	public String getResponseString() {
+		return responseString;
+	}
+
+	public void setResponseString(String responseString) {
+		this.responseString = responseString;
+	}
+
+}
---
+++ src/main/java/com/luv2code/pmo/domain/ReqBody.java
@@ -0,0 +1,13 @@
+package com.luv2code.pmo.domain;
+
+public class ReqBody {
+	private String myFile;
+
+	public String getMyFile() {
+		return myFile;
+	}
+
+	public void setMyFile(String myFile) {
+		this.myFile = myFile;
+	}
+}
